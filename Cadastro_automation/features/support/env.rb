ea32e634334dd0cbeae6e2f0da9bb1eaa0022bb6
Renata Pereira <renata.pereirans@gmail.com>
2019-04-01 08:36:24 -0300

    refactoring

--- Cadastro_automation/features/support/env.rb
+++ Cadastro_automation/features/support/env.rb
@@ -1,3 +1,6 @@
+# language: pt
+# utf-8
+
 require 'rspec'
 require 'cucumber'
 require 'selenium-webdriver'
@@ -5,16 +8,16 @@ require 'capybara'
 require 'capybara/cucumber'
 require 'pry'
 
-#Configurando o driver Capybara
+# Configurando o driver Capybara
 Capybara.register_driver :selenium do |app|
-	Capybara::Selenium::Driver.new(app, :browser => :chrome) 
+  Capybara::Selenium::Driver.new(app, :browser => :chrome) 
 	end
 
-#Setando a configuração do driver como padrão
+# Setando a configuração do driver como padrão
 Capybara.default_driver = :selenium
 
 #timeout padrão na execução
 Capybara.default_max_wait_time = 15
 
 #Maximizar a tela ao iniciar o teste
-Capybara.page.driver.browser.manage.window.maximize	
+Capybara.page.driver.browser.manage.window.maximize
